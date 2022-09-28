3680738f0501905f18949ea51e8130c687e8e1a8
Flogekiller <florian.schnoetzlinger@gmail.com>
2022-09-28 15:54:15 +0200

    set

--- src/com/company/NumberTester.java
+++ src/com/company/NumberTester.java
@@ -2,12 +2,27 @@ package com.company;
 
 public class NumberTester
 {
+    NumberTest oddTester;
+    NumberTester primeTester;
+    NumberTest palindromeTester;
     public NumberTester(String fileName)
     {
 
     }
-    public void setOddEvenTester(NumberTest oddTester)
-    {
+
+    public void setOddTester(NumberTest oddTester) {
         this.oddTester = oddTester;
+    }
+
+    public void setPrimeTester(NumberTester primeTester) {
+        this.primeTester = primeTester;
+    }
+
+    public void setPalindromeTester(NumberTest palindromeTester) {
+        this.palindromeTester = palindromeTester;
+    }
+    public void testFile()
+    {
+        
     }
 }
