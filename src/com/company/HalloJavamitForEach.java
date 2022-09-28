d90199b7a50e228ccbb3df3c2b1b2233f9b22aab
Flogekiller <florian.schnoetzlinger@gmail.com>
2022-09-28 15:39:13 +0200

    normaleForEach

---
+++ src/com/company/HalloJavamitForEach.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HalloJavamitForEach
+{
+
+    public HalloJavamitForEach()
+    {
+        List<String> list = new ArrayList<>();
+        for (String s: list)
+        {
+            System.out.println(s);
+        }
+    }
+}
