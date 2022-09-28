4e599da4ff5e951ffb8e779d2ad04917221c5001
Flogekiller <florian.schnoetzlinger@gmail.com>
2022-09-28 15:48:06 +0200

    NumberTestInterface

---
+++ src/com/company/NumberTest.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public interface NumberTest
+{
+    boolean testNumber(int number);
+}
---
+++ src/com/company/NumberTester.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class NumberTester
+{
+    public NumberTester(String fileName)
+    {
+
+    }
+    public void setOddEvenTester(NumberTest oddTester)
+    {
+        this.oddTester = oddTester;
+    }
+}
