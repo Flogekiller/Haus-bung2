6b66766243a0c6d9b57c2bab0e867e5126bccde0
Flogekiller <florian.schnoetzlinger@gmail.com>
2022-09-29 17:34:51 +0200

    CalculatorsImplement

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Main main = new Main();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1 um Menü anzuzeigen");
+        while(scanner.nextInt() != 4) {
+            System.out.println("Choose calculator: ");
+            System.out.println("1- Relational calculator");
+            System.out.println("2- Vector calculator");
+            System.out.println("3- Complex calculator");
+            System.out.println("4- Exit calculator");
+            int eingabe = scanner.nextInt();
+            switch (eingabe)
+            {
+                case 1:
 
+                    break;
+            }
+
+        }
     }
+
+
 }
