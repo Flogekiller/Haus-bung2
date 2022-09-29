02e5f72ab8f5fcaa5af130fbc4dfeca2ded6328e
Flogekiller <florian.schnoetzlinger@gmail.com>
2022-09-29 17:38:23 +0200

    extraCommit1;D

--- src/com/company/VectorCalculator.java
+++ src/com/company/VectorCalculator.java
@@ -9,6 +9,7 @@ public class VectorCalculator extends AbstractCalculator
 
     @Override
     public Number add(Number a, Number b) {
+        System.out.println("Ich muss noch commits machen1");
         return null;
     }
 
